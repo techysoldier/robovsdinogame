704b91f39b037e5e6cd3967b7430e870d384422a
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-22 10:57:36 -0400

    Update each class with proper dot notation format

--- Robotsvsdino/dino.py
+++ Robotsvsdino/dino.py
@@ -8,23 +8,26 @@ class Dino:
         self.attack_power = Weapon('')
     pass
     def choose_name(self,name): 
-        name = ['littlefoot', 'duckie', 'petree', 'spike']
+        self.choose_name = ['littlefoot', 'duckie', 'petree', 'spike']
         print(input('What is your dino name?'))
-        if input != name: 
+        if input != self.choose_name: 
                 print('Lame, try again')
         if input == 'sarah': 
             print(' No three horns allowed')
         pass
     def choose_weapon (self,attack_choiced): 
         attack_choiced = ['stomp', 'roar','headbut','charge']
-        self.dinoattack = attack_choiced
+        self.choose_weapon = attack_choiced
         print(input('Choose your attack'))
-        print('Initiating', attack_choiced)
+        print('Initiating', self.choose_weapon)
     pass
-    def health_meter(self, health):
-        health == '100'
+    def health_meter(self):
+        self.health_meter == '100'
         print("Your health is currently:") 
-        print(health)
+        print(self.health_meter)
     pass 
-
-
+    def attackpower_amount (self): 
+        attackpower = ['10', '20','50','2']
+        self.attackpower_amount = attackpower
+        print(self.attackpower_amount)
+    pass 
--- Robotsvsdino/robot.py
+++ Robotsvsdino/robot.py
@@ -2,28 +2,28 @@ from unicodedata import name
 from weapon import Weapon
 class Robot:
     def __init__(self,name) -> None:
-        self.nametwo == name
+        self.robot == name
         self.health == '100'
         Weapon == Weapon
     pass 
 
     def robo_name(self, name):
         name = ['terminator', 'R2D2', "R5D4", 'AXL']
-        self.robot == name
+        self.robo_name == name
         print(name)
     pass
 
 
     def choose_weapon (self,attack_choicer): 
         attack_choicer = ['blaster', 'rocket launcher','muay thai kick','overload']
-        self.roboattack = attack_choicer
+        self.choose_weapon = attack_choicer
         print(input('Choose your attack'))
         print('Initiating', attack_choicer)
     pass
 
-    def health_meter(self, health):
-        self.health == '100'
+    def health_meter(self):
+        self.health_meter == '100'
         print("Your health is currently:") 
-        print(health)
+        print(self.health_meter)
     pass
     
